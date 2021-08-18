cae94b702e5feecaca0323f4c04ea8f130da070c
LoneRifle <LoneRifle@users.noreply.github.com>
2021-08-18 12:42:36 +0800

    build(ci): put reCAPTCHA site key in build, not server
    - remove `recaptchaSiteKey` from server config, not used
    - supply `REACT_APP_RECAPTCHA_SITE_KEY` as a docker build arg
    - have GitHub Actions select the env-dependent key at build time

--- server/src/bootstrap/config/recaptcha.ts
+++ server/src/bootstrap/config/recaptcha.ts
@@ -1,17 +1,10 @@
 import convict, { Schema } from 'convict'
 
 export type RecaptchaConfig = {
-  recaptchaSiteKey: string
   recaptchaSecretKey: string
 }
 
 const recaptchaSchema: Schema<RecaptchaConfig> = {
-  recaptchaSiteKey: {
-    doc: 'Recaptcha key for client to generate user response',
-    format: String,
-    default: '',
-    env: 'REACT_APP_RECAPTCHA_SITE_KEY',
-  },
   recaptchaSecretKey: {
     doc: 'Recaptcha key for server to verify user response',
     format: String,
