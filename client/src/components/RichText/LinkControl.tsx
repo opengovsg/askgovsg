61cb448e882de053e7e8a6c74db1449857f83781
nauynix <nauynix@gmail.com>
2021-08-19 15:04:39 +0800

    fix: remove nested form of linkcontrol

--- client/src/components/RichText/LinkControl.tsx
+++ client/src/components/RichText/LinkControl.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, HStack, Input, Text } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { MouseEventHandler, useState } from 'react'
 import { BiLink } from 'react-icons/bi'
 import { useStyledToast } from '../StyledToast/StyledToast'
 import styles from './RichTextEditor.module.scss'
@@ -50,43 +50,41 @@ export const LinkControl = ({
     e.stopPropagation()
   }
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  const handleSubmit: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault()
     e.stopPropagation()
     onChange('link', title, url, '_blank')
   }
 
   const renderModal = () => (
-    <div onClick={stopPropagation}>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <Box w="376px" h="128px" p="24px">
-          <Text textStyle="subhead1" color="secondary.700">
-            Add Target Link
-          </Text>
-          <HStack spacing="16px" my="12px">
-            <Input
-              value={url}
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setURL(e.target.value)
-              }
-            />
-            <Button
-              textStyle="body1"
-              backgroundColor="primary.500"
-              color="white"
-              type="submit"
-              name="link_button"
-              isDisabled={!title || !url}
-              _hover={undefined}
-              _active={undefined}
-              _focus={undefined}
-            >
-              Save
-            </Button>
-          </HStack>
-        </Box>
-      </form>
+    <div onClick={stopPropagation} className={styles.form}>
+      <Box w="376px" h="128px" p="24px">
+        <Text textStyle="subhead1" color="secondary.700">
+          Add Target Link
+        </Text>
+        <HStack spacing="16px" my="12px">
+          <Input
+            value={url}
+            type="text"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setURL(e.target.value)
+            }
+          />
+          <Button
+            textStyle="body1"
+            backgroundColor="primary.500"
+            color="white"
+            onClick={handleSubmit}
+            name="link_button"
+            isDisabled={!title || !url}
+            _hover={undefined}
+            _active={undefined}
+            _focus={undefined}
+          >
+            Save
+          </Button>
+        </HStack>
+      </Box>
     </div>
   )
   return (
