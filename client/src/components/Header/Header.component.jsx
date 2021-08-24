644c0295e9c5578c0f244370f58021a2e52794ae
nauynix <nauynix@gmail.com>
2021-08-24 11:22:21 +0800

    fix: align mobile header view with design

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -1,12 +1,4 @@
-import {
-  Box,
-  Divider,
-  Flex,
-  HStack,
-  Image,
-  Stack,
-  Text,
-} from '@chakra-ui/react'
+import { Box, Flex, Image, Spacer, Stack, Text } from '@chakra-ui/react'
 import { useQuery } from 'react-query'
 import { Link, matchPath, useLocation } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/LogoAlpha.svg'
@@ -41,6 +33,42 @@ const Header = () => {
     () => getAgencyByShortName({ shortname: agencyShortName }),
     { enabled: !!agencyShortName },
   )
+  const isMobile = screen.width < 768
+
+  const agencyLink = (
+    <Link to={agency ? `/agency/${agency.shortname}` : '/'}>
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        align={{ base: 'flex-start', md: 'center' }}
+      >
+        <Box pr="15px" mb={{ base: '0px', md: '6px' }}>
+          <Logo />
+        </Box>
+        {agency ? (
+          <>
+            <Text
+              d={{ base: 'none', md: 'block' }}
+              pr={{ base: 0, md: 5 }}
+              textStyle="h4"
+              fontWeight={300}
+              color="white"
+            >
+              |
+            </Text>
+            <Text
+              position={{ base: 'relative', md: 'static' }}
+              top={{ base: '-6px', md: 0 }}
+              textStyle="h4"
+              fontWeight={400}
+              color="white"
+            >
+              {agency.longname}
+            </Text>
+          </>
+        ) : null}
+      </Stack>
+    </Link>
+  )
 
   const authLinks = (
     <Flex align="center">
@@ -65,63 +93,41 @@ const Header = () => {
   )
 
   return (
-    <Flex
-      background="primary.500"
-      display="flex"
-      justify="space-between"
-      align="center"
-      px="32px"
-      py="15px"
-      height={{ base: '122px', md: '74px' }}
-    >
-      <Stack
-        direction={{ base: 'column', md: 'row' }}
-        textDecor="none"
-        align={{ base: 'flex-start', md: 'center' }}
-        position="relative"
-        top="8px"
-      >
-        {/* Show tag panel mobile only on mobile and homepage*/}
-        {isHomePage && (
-          <Box d={{ base: 'block', xl: 'none' }}>
-            <TagPanelMobile />
-          </Box>
-        )}
-        <Link to={agency ? `/agency/${agency.shortname}` : '/'}>
-          <Stack
-            direction={{ base: 'column', md: 'row' }}
-            align={{ base: 'flex-start', md: 'center' }}
-          >
-            <Box pr="15px" mb={{ base: '0px', md: '6px' }}>
-              <Logo />
-            </Box>
-            {agency ? (
-              <>
-                <Text
-                  d={{ base: 'none', md: 'block' }}
-                  pr={{ base: 0, md: 5 }}
-                  textStyle="h4"
-                  fontWeight={300}
-                  color="white"
-                >
-                  |
-                </Text>
-                <Text
-                  position={{ base: 'relative', md: 'static' }}
-                  top={{ base: '-6px', md: 0 }}
-                  textStyle="h4"
-                  fontWeight={400}
-                  color="white"
-                >
-                  {agency.longname}
-                </Text>
-              </>
-            ) : null}
-          </Stack>
-        </Link>
-      </Stack>
-      {user && authLinks}
-    </Flex>
+    <Box px={8} py={4} background="primary.500" textDecor="none">
+      {(() => {
+        if (isMobile) {
+          return (
+            <Stack>
+              <Flex>
+                {/* Show tag panel mobile only on mobile and homepage*/}
+                {isHomePage && (
+                  <Box mr={5}>
+                    <TagPanelMobile />
+                  </Box>
+                )}
+                <Spacer />
+                {user && authLinks}
+              </Flex>
+              {agencyLink}
+            </Stack>
+          )
+        } else {
+          return (
+            <Flex align="center">
+              {/* Show tag panel mobile only on mobile and homepage*/}
+              {isHomePage && (
+                <Box mr={5} d={{ base: 'block', xl: 'none' }}>
+                  <TagPanelMobile />
+                </Box>
+              )}
+              {agencyLink}
+              <Spacer />
+              {user && authLinks}
+            </Flex>
+          )
+        }
+      })()}
+    </Box>
   )
 }
 
