907e9e969777250eef03cf1a3939063601a254e9
nauynix <nauynix@gmail.com>
2021-08-24 15:01:01 +0800

    feat: add Pagination Component from FormSG

--- client/src/theme/components/index.ts
+++ client/src/theme/components/index.ts
@@ -1,7 +1,9 @@
 import { Button } from './Button'
 import { StyledToast } from './StyledToast'
+import { Pagination, PAGINATION_THEME_KEY } from './Pagination'
 
 export const components = {
   Button,
   StyledToast,
+  [PAGINATION_THEME_KEY]: Pagination,
 }
