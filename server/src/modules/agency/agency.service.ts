c3744171ad7e73ff76294e66809039b54c616361
Feng Xinyuan <nauynix@gmail.com>
2021-08-31 16:58:16 +0800

    refactor(agency): type controllers as ControllerHandler (#181)
    * refactor(agency): type controllers as controllerHandler
    
    * docs(agency): add jsdocs to agency module
    
    * docs: update readme include agency api

--- server/src/modules/agency/agency.service.ts
+++ server/src/modules/agency/agency.service.ts
@@ -3,6 +3,11 @@ import { Agency as AgencyType } from '../../models/agencies.model'
 import { AgencyQuery } from '../../types/agency-type'
 
 export class AgencyService {
+  /**
+   * Find an agency by their shortname or longname
+   * @param query agency's shortname or longname
+   * @returns agency if found, else null
+   */
   findOneByShortName = async (
     query: AgencyQuery,
   ): Promise<AgencyType | null> => {
@@ -16,8 +21,13 @@ export class AgencyService {
     }
   }
 
-  // tried integrating into one function but params can't tell
-  // the diff between string or number
+  /**
+   * Find an agency by their id
+   * tried integrating into one function but params can't tell
+   * the diff between string or number
+   * @param agencyId Agency's id
+   * @returns agency if found, else null
+   */
   findOneById = async (agencyId: number): Promise<AgencyType | null> => {
     const agency = await Agency.findOne({
       where: { id: agencyId },
