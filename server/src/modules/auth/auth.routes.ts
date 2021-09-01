31f19fbf6e265f0b3fbc51e7873bca8de19f4238
nauynix <nauynix@gmail.com>
2021-09-01 14:49:17 +0800

    chore: update readme

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -14,25 +14,23 @@ export const routeAuth = ({
 
   /**
    * Fetch logged in user details
-   * @route      GET /api/auth
+   * @route   GET /api/auth
    * @returns 200 with user details
    * @returns 401 if user not signed in
    * @returns 500 if database error
-   * @access     Private
+   * @access  Private
    */
   router.get('/', middleware.authenticate, controller.loadUser)
 
   /**
    * Verify jwt received by the user and set the JWT
-   * @route      POST /api/auth/verifyotp
-   * @body email email of user
-   * @body otp otp of user
+   * @route   POST /api/auth/verifyotp
    * @returns 200 with JWT if successful login
    * @returns 400 if validation of body fails
    * @returns 401 if no otp was sent for user
    * @returns 401 if wrong otp
    * @returns 500 if database error
-   * @access     Private
+   * @access  Private
    */
   router.post(
     '/verifyotp',
@@ -51,12 +49,12 @@ export const routeAuth = ({
 
   /**
    * Send login otp to specified email
-   * @route      POST /api/auth/sendotp
+   * @route   POST /api/auth/sendotp
    * @returns 200 if OTP sent
    * @returns 400 if email is invalid
    * @returns 400 if email does not belong to a user
    * @returns 500 if OTP failed to send
-   * @access     Private
+   * @access  Private
    */
   router.post(
     '/sendotp',
