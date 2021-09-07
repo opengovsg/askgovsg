7c14d945123be359d6f4b1feb0f7bf22734b4177
Lin Huiqing <linhuiqing@Lins-MacBook-Pro.local>
2021-09-07 09:02:52 +0800

    issue 8: convert NotFound page to Chakra, rm box__ghost

--- client/src/pages/NotFound/NotFound.component.jsx
+++ client/src/pages/NotFound/NotFound.component.jsx
@@ -1,52 +1,41 @@
 import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import { Spacer } from '@chakra-ui/react'
+import { Link as ReachLink } from 'react-router-dom'
+import {
+  Spacer,
+  Heading,
+  Text,
+  Button,
+  Center,
+  Flex,
+  Container,
+} from '@chakra-ui/react'
 
 import './NotFound.styles.scss'
 import { BackToHome } from '../../components/BackToHome/BackToHome'
 
 const NotFound = () => {
   return (
-    <Fragment>
+    <Fragment centerContent>
       <BackToHome />
-      <div className="page">
-        <div className="box">
-          <div className="box__ghost">
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-            <div className="symbol"></div>
-
-            <div className="box__ghost-container">
-              <div className="box__ghost-eyes">
-                <div className="box__eye-left"></div>
-                <div className="box__eye-right"></div>
-              </div>
-              <div className="box__ghost-bottom">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
-            </div>
-            <div className="box__ghost-shadow"></div>
-          </div>
-          <div className="box__description">
-            <div className="box__description-container">
-              <div className="box__description-title fc-black-800">Whoops!</div>
-              <div className="box__description-text fc-black-500">
-                It seems like we couldn't find the page you were looking for
-              </div>
-            </div>
-            <Link to="/" className="box__button">
-              Back to home page
-            </Link>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Container>
+          <Flex direction="column">
+            <Heading align="center" size="4xl">
+              Not Found
+            </Heading>
+            <Spacer />
+            <Text align="center">Whoops!</Text>
+            <Text align="center">
+              It seems like we couldn't find the page you were looking for :(
+            </Text>
+            <Center>
+              <Button as={ReachLink} to="/">
+                Back to home page
+              </Button>
+            </Center>
+          </Flex>
+        </Container>
+      </Container>
       <Spacer minH={20} />
     </Fragment>
   )
