3dfac96bd00f7b4dcfe3635aab65215aaf6b8f85
LinHuiqing <huiqing@open.gov.sg>
2021-09-07 17:09:07 +0800

    fix(auth): modify url to direct user to for fallback to login url

--- server/src/modules/mail/mail.util.ts
+++ server/src/modules/mail/mail.util.ts
@@ -1,6 +1,6 @@
 export const renderLoginOtpBody = (otp: string, ip: string): string =>
   `Your OTP for AskGov is <b>${otp}</b>. Please use this to login to your AskGov account.<br><br>\
-If your OTP does not work, please request for a new OTP at <a href="https://ask.gov.sg/">https://ask.gov.sg/</a>.<br><br>\
+If your OTP does not work, please request for a new OTP at <a href="https://ask.gov.sg/login">https://ask.gov.sg/login</a>.<br><br>\
 This login attempt was made from the IP: <u>${ip}</u>. If you did not attempt to log in to AskGov, you may choose to investigate this IP to address further. <br><br>\
 <br>\
 The AskGov Support Team`
