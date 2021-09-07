45fbd15c881454887702d883b645224108cce538
Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>
2021-09-07 13:35:37 +0800

    refactor(notfound): move to Chakra, remove unneeded CSS (#235)
    - convert NotFound page to use Chakra
    - remove `box`, `box__*` styling
    - trim superfluous text to keep message short and clear
    - take opportunity to quickly change to TypeScript
    
    Co-authored-by: Lin Huiqing <linhuiqing@Lins-MacBook-Pro.local>

---
+++ client/src/pages/NotFound/NotFound.component.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment } from 'react'
+import { Link as ReachLink } from 'react-router-dom'
+import {
+  Spacer,
+  Heading,
+  Text,
+  Button,
+  Center,
+  Flex,
+  Container,
+} from '@chakra-ui/react'
+
+import { BackToHome } from '../../components/BackToHome/BackToHome'
+
+const NotFound = (): JSX.Element => {
+  return (
+    <Fragment>
+      <BackToHome />
+      <Container>
+        <Container>
+          <Flex direction="column">
+            <Heading align="center" size="4xl">
+              Not Found
+            </Heading>
+            <Spacer minH={10} />
+            <Text align="center">
+              It seems like we couldn't find the page you were looking for.
+            </Text>
+            <Spacer minH={10} />
+            <Center>
+              <Button as={ReachLink} to="/">
+                Back to home page
+              </Button>
+            </Center>
+          </Flex>
+        </Container>
+      </Container>
+      <Spacer minH={20} />
+    </Fragment>
+  )
+}
+
+export default NotFound
