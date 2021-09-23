9f4f5606d2588ddc29e9d88d6f3173b785983e7f
LinHuiqing <huiqing@open.gov.sg>
2021-09-23 13:52:10 +0800

    feat(breakpoints): add breakpoints for devices
    added breakpoints for devices so specifications for mobile, table and desktop can be simply added
    with:
    `{ base: <specs for mobile>, tablet: <specs for tablet>, desktop: <specs for desktop> }`

--- client/src/theme/breakpoints.ts
+++ client/src/theme/breakpoints.ts
@@ -1,7 +1,13 @@
+// Note the following breakpoints for different devices:
+// - mobile: 479px and below
+// - tablet: 480-1439px
+// - desktop: 1440px and above
 export const breakpoints = {
   xs: '360px',
   sm: '480px',
   md: '768px',
   lg: '1024px',
   xl: '1440px',
+  tablet: '480px', // should be same as sm
+  desktop: '1440px', // should be same as xl
 }
