8235207e096bcb9c1ffed3c034ee5a18e6206135
LinHuiqing <huiqing@open.gov.sg>
2021-09-27 12:01:27 +0800

    fix(breakpoints): use createBreakpoints to extend theme breakpoints

--- client/src/theme/breakpoints.ts
+++ client/src/theme/breakpoints.ts
@@ -1,7 +1,9 @@
-export const breakpoints = {
+import { createBreakpoints } from '@chakra-ui/theme-tools'
+
+export const breakpoints = createBreakpoints({
   xs: '22.5em',
   sm: '30em',
   md: '48em',
   lg: '64em',
   xl: '90em',
-}
+})
