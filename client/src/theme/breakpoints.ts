821781c60e0f43139fa40dc06412fe91fbdc760a
Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>
2021-09-28 10:25:49 +0800

    feat(masthead): update to new masthead with more details (#374)
    * feat(masthead): update themes related to masthead
    
    - update colors related to masthead
    - update text styles related to masthead
    
    - refactor Masthead component to use Chakra
    - implement new masthead with dropdown
    - implemented based on specs in Design System:
    https://www.figma.com/file/yATTXTZ5Goy9XDy5qooSO3?node-id=2153:4186#111044594
    
    - Boxicon icons used in svg as they were not included in react-icons yet
      - bank
      - lock-alt
    
    - comment on breakpoints for devices
    - display specs for devices:
      - mobile: 479px and below
      - tablet: 480-1439px
      - desktop: 1440px and above
    - In other words, use the following format for responsive styling: `{ base: <specs for mobile>, sm: <specs for tablet>, xl: <specs for desktop> }`

--- client/src/theme/breakpoints.ts
+++ client/src/theme/breakpoints.ts
@@ -1,5 +1,8 @@
 import { createBreakpoints } from '@chakra-ui/theme-tools'
-
+// Note the following breakpoints for different devices:
+// - mobile: 479px and below
+// - tablet: 480-1439px
+// - desktop: 1440px and above
 export const breakpoints = createBreakpoints({
   xs: '22.5em',
   sm: '30em',
