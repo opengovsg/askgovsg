41c0f9cc55752931c7a5f1a6efbcce090e84de99
LinHuiqing <huiqing@open.gov.sg>
2021-09-30 08:56:17 +0800

    refactor(pagetitle): migrate pagetitle to typescript
    - migrate to typescript
    - add description field to page title
    - add package installation for @types/react-helmet

---
+++ client/src/components/PageTitle/PageTitle.component.tsx
@@ -0,0 +1,24 @@
+import Helmet from 'react-helmet'
+
+const PageTitle = ({
+  title,
+  description,
+}: {
+  title: string | undefined
+  description: string | undefined
+}) => {
+  const defaultTitle = 'AskGov'
+  const defaultDescription = 'Answers from the Singapore Government'
+
+  return (
+    <Helmet>
+      <title>{title ? title : defaultTitle}</title>
+      <meta
+        name="description"
+        content={description ? description : defaultDescription}
+      />
+    </Helmet>
+  )
+}
+
+export default PageTitle
