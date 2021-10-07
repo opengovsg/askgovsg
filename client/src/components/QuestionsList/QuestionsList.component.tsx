9b76b4d9c2c84a74a354d84e6065e76cc507e52e
Lin Huiqing <37061143+LinHuiqing@users.noreply.github.com>
2021-10-07 16:57:32 +0800

    feat(view-all): add 'View all questions' button on main (and agency) pages (#514)
    - on main (/agency) pages: show top 10 with the 'View all questions' button at the end
    - on tag-specific page/ when 'View all questions' button is clicked: paginate with 30 questions per page
    
    re #352

--- client/src/components/QuestionsList/QuestionsList.component.tsx
+++ client/src/components/QuestionsList/QuestionsList.component.tsx
@@ -1,27 +1,38 @@
 import { Center } from '@chakra-ui/layout'
-import { useState } from 'react'
+import { Button, Text } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
 import { useQuery } from 'react-query'
 import { listPosts, LIST_POSTS_QUERY_KEY } from '../../services/PostService'
+import { mergeTags } from '../../util/tagsmerger'
+import Pagination from '../Pagination'
 import PostListComponent from '../PostList/PostList.component'
 import Spinner from '../Spinner/Spinner.component'
-import './QuestionsList.styles.scss'
-import Pagination from '../Pagination'
 
 interface QuestionsListProps {
   sort: string
+  agency: string
   tags: string
   pageSize: number
 }
 
 const QuestionsList = ({
   sort,
+  agency,
   tags,
   pageSize,
 }: QuestionsListProps): JSX.Element => {
   // Pagination
   const [page, setPage] = useState(1)
+  const [expanded, setExpanded] = useState(tags.length > 0)
+
+  useEffect(() => {
+    setExpanded(tags.length > 0)
+  }, [tags])
+
+  const agencyAndTags = mergeTags(agency, tags)
+
   const { data, isLoading } = useQuery(
-    [LIST_POSTS_QUERY_KEY, { sort, tags, page, pageSize }],
+    [LIST_POSTS_QUERY_KEY, { sort, agencyAndTags, page, pageSize }],
     () => listPosts(sort, tags, page, pageSize),
     { keepPreviousData: true },
   )
@@ -37,18 +48,29 @@ const QuestionsList = ({
   ) : (
     <>
       <PostListComponent
-        posts={data?.posts}
+        posts={data?.posts.slice(0, expanded ? pageSize : 10)}
         defaultText={undefined}
         alertIfMoreThanDays={undefined}
         showViews={undefined}
       />
       <Center my={5}>
-        <Pagination
-          totalCount={data?.totalItems ?? 0}
-          pageSize={pageSize}
-          onPageChange={handlePageChange}
-          currentPage={page}
-        ></Pagination>
+        {expanded ? (
+          <Pagination
+            totalCount={data?.totalItems ?? 0}
+            pageSize={pageSize}
+            onPageChange={handlePageChange}
+            currentPage={page}
+          />
+        ) : (
+          <Button
+            variant="outline"
+            color="primary.500"
+            borderColor="primary.500"
+            onClick={() => setExpanded(true)}
+          >
+            <Text textStyle="subhead-1">View all questions</Text>
+          </Button>
+        )}
       </Center>
     </>
   )
