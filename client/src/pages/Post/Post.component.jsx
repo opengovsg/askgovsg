20d95808f766269febee239fa983276ed44883f9
LinHuiqing <huiqing@open.gov.sg>
2021-10-12 15:01:41 +0800

    feat(breadcrumbs): implement breadcrumbs for users to nagivate from posts
    - create NavBreadcrumb component
    - replace back button in Post with NavBreadcrumb
    - update primary 600 color with color in Design Master v1
    
    re #552

--- client/src/pages/Post/Post.component.jsx
+++ client/src/pages/Post/Post.component.jsx
@@ -1,13 +1,14 @@
 import { Center, Flex, Spacer, Stack, Text, VStack } from '@chakra-ui/layout'
 import { format, utcToZonedTime } from 'date-fns-tz'
+import { useEffect, useRef } from 'react'
 import { BiXCircle } from 'react-icons/bi'
 import { useQuery } from 'react-query'
 import { Link, useParams } from 'react-router-dom'
 import sanitizeHtml from 'sanitize-html'
 import { PostStatus, TagType } from '~shared/types/base'
-import { BackToHome } from '../../components/BackToHome/BackToHome'
 import CitizenRequest from '../../components/CitizenRequest/CitizenRequest.component'
 import EditButton from '../../components/EditButton/EditButton.component'
+import { NavBreadcrumb } from '../../components/NavBreadcrumb/NavBreadcrumb'
 import PageTitle from '../../components/PageTitle/PageTitle.component'
 import Spinner from '../../components/Spinner/Spinner.component'
 import { useAuth } from '../../contexts/AuthContext'
@@ -79,6 +80,33 @@ const Post = () => {
     { enabled: Boolean(post?.id) },
   )
 
+  const breadcrumbContentRef = useRef([])
+
+  useEffect(() => {
+    if (post) {
+      let foundAgency = undefined
+      let foundTopic = undefined
+      for (const postTag of post.tags) {
+        if (!foundAgency && postTag.tagType === TagType.Agency) {
+          foundAgency = {
+            text: postTag.tagname.toUpperCase(),
+            link: `/agency/${postTag.tagname}`,
+          }
+        }
+        if (!foundTopic && postTag.tagType === TagType.Topic) {
+          foundTopic = {
+            text: postTag.tagname,
+            link: `/agency/${agencyShortName}?tags=${postTag.tagname}`,
+          }
+        }
+        if (foundTopic && foundAgency) break
+      }
+      breadcrumbContentRef.current = []
+      if (foundAgency) breadcrumbContentRef.current.push(foundAgency)
+      if (foundTopic) breadcrumbContentRef.current.push(foundTopic)
+    }
+  }, [post?.tags])
+
   return isLoading ? (
     <Spinner centerHeight="200px" />
   ) : (
@@ -106,12 +134,14 @@ const Post = () => {
         >
           <div className="post-page">
             <Flex align="center">
-              <Flex
-                mt={{ base: '32px', sm: '60px' }}
-                mb={{ base: '32px', sm: '50px' }}
-              >
-                <BackToHome mainPageName={agencyShortName} />
-              </Flex>
+              {breadcrumbContentRef.current.length > 0 ? (
+                <Flex
+                  mt={{ base: '32px', sm: '60px' }}
+                  mb={{ base: '32px', sm: '50px' }}
+                >
+                  <NavBreadcrumb navOrder={breadcrumbContentRef.current} />
+                </Flex>
+              ) : null}
               <Spacer />
               {isAgencyMember && (
                 <div className="post-side-with-edit">
