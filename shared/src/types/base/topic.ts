e91ef0ebc1f59f2fc75a6c2aa896b4db9d45c4f5
Wan Ling Thoo <wanling@open.gov.sg>
2021-10-15 11:04:34 +0800

    fix(topic): add nullable to topic base type fields
    make description and parentId nullable

--- shared/src/types/base/topic.ts
+++ shared/src/types/base/topic.ts
@@ -3,8 +3,8 @@ import { BaseModel } from './common'
 
 export const Topic = BaseModel.extend({
   name: z.string(),
-  description: z.string(),
-  parentId: z.number(),
+  description: z.string().nullable(),
+  parentId: z.number().nullable(),
   agencyId: z.number(),
 })
 
