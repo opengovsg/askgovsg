752e9475edc23cd6a0e1dbc8b63ce5d52ad6a600
Wan Ling Thoo <wanling@open.gov.sg>
2021-10-18 09:58:16 +0800

    test(topics): add unit tests
    re #398

---
+++ server/src/modules/topics/__tests__/topics.routes.spec.ts
@@ -0,0 +1,119 @@
+import { Topic, Agency } from '~shared/types/base'
+import { routeTopics } from '../topics.routes'
+import { TopicsService, TopicWithChildRelations } from '../topics.service'
+import { TopicsController } from '../topics.controller'
+import { ControllerHandler } from '../../../types/response-handler'
+import { createTestDatabase, getModel, ModelName } from '../../../util/jest-db'
+import { Sequelize, Model } from 'sequelize'
+import { ModelDef } from '../../../types/sequelize'
+import express from 'express'
+import supertest, { SuperTest, Test } from 'supertest'
+import { StatusCodes } from 'http-status-codes'
+
+describe('/topics', () => {
+  const path = '/topics'
+  let mockAgency: Agency
+  let mockTopic1: Topic
+  let mockTopic2: Topic
+  let mockTopic3: Topic
+  let mockTopic4: Topic
+  let mockTopics: TopicWithChildRelations[]
+  let db: Sequelize
+  let request: SuperTest<Test>
+  let Agency: ModelDef<Agency>
+  let Topic: ModelDef<Topic>
+
+  // Set up service, controller and route
+  const authService = {
+    checkIfWhitelistedOfficer: jest.fn(),
+    hasPermissionToAnswer: jest.fn(),
+    getDisallowedTagsForUser: jest.fn(),
+    verifyUserCanViewPost: jest.fn(),
+    isOfficerEmail: jest.fn(),
+    verifyUserCanModifyTopic: jest.fn(),
+    verifyUserInAgency: jest.fn(),
+  }
+
+  const authenticate: ControllerHandler = (req, res, next) => {
+    req.user = { id: 1 }
+    next()
+  }
+
+  const authMiddleware = {
+    authenticate,
+  }
+
+  beforeAll(async () => {
+    db = await createTestDatabase()
+    Topic = getModel<Topic & Model>(db, ModelName.Topic)
+    Agency = getModel<Agency & Model>(db, ModelName.Agency)
+
+    mockAgency = await Agency.create({
+      shortname: 'was',
+      longname: 'Work Allocation Singapore',
+      email: 'enquiries@was.gov.sg',
+      logo: 'https://logos.ask.gov.sg/askgov-logo.svg',
+      noEnquiriesMessage: null,
+      website: null,
+      displayOrder: null,
+    })
+
+    mockTopic1 = await Topic.create({
+      name: '1',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: null,
+    })
+    mockTopic2 = await Topic.create({
+      name: '2',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: mockTopic1.id,
+    })
+    mockTopic3 = await Topic.create({
+      name: '3',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: mockTopic2.id,
+    })
+    mockTopic4 = await Topic.create({
+      name: '4',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: null,
+    })
+
+    mockTopics = [
+      {
+        ...mockTopic1,
+        children: [{ ...mockTopic2, children: [mockTopic3] }],
+      },
+      mockTopic4,
+    ]
+
+    const topicsService = new TopicsService({ Topic })
+    const controller = new TopicsController({
+      authService,
+      topicsService,
+    })
+
+    const router = routeTopics({ controller, authMiddleware })
+    const app = express()
+    app.use(authenticate)
+    app.use(path, router)
+    request = supertest(app)
+  })
+
+  afterAll(async () => {
+    await db.close()
+  })
+
+  describe(`/:agencyId`, () => {
+    it('returns 200 and nested list of posts corresponding to an agency on request', async () => {
+      const { id } = mockAgency
+      const response = await request.get(`${path}/${id}`)
+      expect(response.status).toEqual(StatusCodes.OK)
+      expect(response.body.length).toStrictEqual(mockTopics.length)
+    })
+  })
+})
---
+++ server/src/modules/topics/__tests__/topics.service.spec.ts
@@ -0,0 +1,102 @@
+import { TopicsService, TopicWithChildRelations } from '../topics.service'
+import { createTestDatabase, getModel, ModelName } from '../../../util/jest-db'
+import { Sequelize, Model } from 'sequelize'
+import { Topic, Agency } from '~shared/types/base'
+import { ModelDef } from '../../../types/sequelize'
+import { MissingTopicError } from '../topics.errors'
+
+describe('TopicsService', () => {
+  let db: Sequelize
+  let Topic: ModelDef<Topic>
+  let mockTopic1: Topic
+  let mockTopic2: Topic
+  let mockTopic3: Topic
+  let mockTopic4: Topic
+  let mockTopics: TopicWithChildRelations[]
+  let topicService: TopicsService
+  let Agency: ModelDef<Agency>
+  let mockAgency: Agency
+
+  beforeAll(async () => {
+    db = await createTestDatabase()
+    Topic = getModel<Topic & Model>(db, ModelName.Topic)
+    Agency = getModel<Agency & Model>(db, ModelName.Agency)
+
+    mockAgency = await Agency.create({
+      shortname: 'was',
+      longname: 'Work Allocation Singapore',
+      email: 'enquiries@was.gov.sg',
+      logo: 'https://logos.ask.gov.sg/askgov-logo.svg',
+      noEnquiriesMessage: null,
+      website: null,
+      displayOrder: null,
+    })
+
+    mockTopic1 = await Topic.create({
+      name: '1',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: null,
+    })
+    mockTopic2 = await Topic.create({
+      name: '2',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: mockTopic1.id,
+    })
+    mockTopic3 = await Topic.create({
+      name: '3',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: mockTopic2.id,
+    })
+    mockTopic4 = await Topic.create({
+      name: '4',
+      description: '',
+      agencyId: mockAgency.id,
+      parentId: null,
+    })
+
+    mockTopics = [
+      {
+        ...mockTopic1,
+        children: [{ ...mockTopic2, children: [mockTopic3] }],
+      },
+      mockTopic4,
+    ]
+
+    topicService = new TopicsService({ Topic })
+  })
+
+  afterAll(async () => {
+    await db.close()
+  })
+
+  const expectTopicMatch = (
+    actualTopics: TopicWithChildRelations[],
+    mockTopics: TopicWithChildRelations[],
+  ) => {
+    expect(actualTopics.length).toStrictEqual(mockTopics.length)
+    expect(actualTopics[0].id).toStrictEqual(mockTopics[0].id)
+    expect(actualTopics[0].name).toStrictEqual(mockTopics[0].name)
+    expect(actualTopics[0].description).toStrictEqual(mockTopics[0].description)
+    expect(actualTopics[0].parentId).toStrictEqual(mockTopics[0].parentId)
+    expect(actualTopics[0].children?.[0].id).toStrictEqual(
+      mockTopics[0].children?.[0].id,
+    )
+  }
+
+  describe('listTopicsUsedByAgency', () => {
+    it('lists topics given an agency id', async () => {
+      const { id } = mockAgency
+      const actualTopics = await topicService.listTopicsUsedByAgency(id)
+      expectTopicMatch(actualTopics._unsafeUnwrap(), mockTopics)
+    })
+
+    it('returns MissingTopicError on non-existing agency', async () => {
+      const id = mockAgency.id + 20
+      const actualTopic = await topicService.listTopicsUsedByAgency(id)
+      expect(actualTopic._unsafeUnwrapErr()).toEqual(new MissingTopicError())
+    })
+  })
+})
