0b1c6a5d698f5f5f02042c7f007bdff0eacd304c
LinHuiqing <huiqing@open.gov.sg>
2021-11-09 18:05:36 +0800

    feat(search): load search-related env vars on searchConfig
    - new env vars:
        - SEARCH_HOST
        - SEARCH_USERNAME
        - SEARCH_PASSWORD
        - SEARCH_POST

---
+++ server/src/bootstrap/config/search.ts
@@ -0,0 +1,42 @@
+import convict, { Schema } from 'convict'
+import { url } from 'convict-format-with-validator'
+
+convict.addFormat(url)
+
+export type SearchConfig = {
+  host: string
+  username: string
+  password: string
+  port: number
+}
+
+const searchSchema: Schema<SearchConfig> = {
+  host: {
+    doc: 'Host for opensearch service',
+    format: String,
+    default: 'localhost',
+    env: 'SEARCH_HOST',
+  },
+  username: {
+    doc: 'Username for opensearch service',
+    format: String,
+    default: 'admin',
+    env: 'SEARCH_USERNAME',
+  },
+  password: {
+    doc: 'Password for opensearch service',
+    format: String,
+    default: 'admin',
+    env: 'SEARCH_PASSWORD',
+  },
+  port: {
+    doc: 'Port for opensearch service',
+    format: 'port',
+    default: 9200,
+    env: 'SEARCH_POST',
+  },
+}
+
+export const searchConfig = convict(searchSchema)
+  .validate({ allowed: 'strict' })
+  .getProperties()
