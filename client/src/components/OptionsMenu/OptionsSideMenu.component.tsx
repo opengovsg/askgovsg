104f563a19a122aa011e2f96e8ac7bbc612aa547
christopherlim98 <41635847+christopherlim98@users.noreply.github.com>
2022-01-01 23:25:03 +0800

    feat(storybook/optionssidemenu): add story for optionssidemenu with refactor

--- client/src/components/OptionsMenu/OptionsSideMenu.component.tsx
+++ client/src/components/OptionsMenu/OptionsSideMenu.component.tsx
@@ -1,6 +1,6 @@
 import { Box, Flex, Text, useMultiStyleConfig } from '@chakra-ui/react'
 import * as FullStory from '@fullstory/browser'
-import { LegacyRef, useEffect, useState, ReactElement, createRef } from 'react'
+import { LegacyRef, useState, ReactElement, createRef } from 'react'
 import { useQuery } from 'react-query'
 import { Link as RouterLink } from 'react-router-dom'
 import { useGoogleAnalytics } from '../../contexts/googleAnalytics'
@@ -11,24 +11,18 @@ import {
   getTopicsUsedByAgency,
   GET_TOPICS_USED_BY_AGENCY_QUERY_KEY,
 } from '../../services/TopicService'
-import {
-  getRedirectURLTopics,
-  isSpecified,
-  getTopicsQuery,
-} from '../../util/urlparser'
+import { getRedirectURLTopics } from '../../util/urlparser'
 import { bySpecifiedOrder } from './util'
 
-const OptionsSideMenu = ({ agency }: { agency?: Agency }): ReactElement => {
-  const [hasTopicsKey, setHasTopicsKey] = useState(false)
-
-  const [queryState, setQueryState] = useState('')
-  const styles = useMultiStyleConfig('OptionsMenu', { hasTopicsKey })
-
-  useEffect(() => {
-    setQueryState(getTopicsQuery(location.search))
-    const topicsSpecified = isSpecified(location.search, 'topics')
-    setHasTopicsKey(topicsSpecified)
-  })
+const OptionsSideMenu = ({
+  agency,
+  queryStateProp,
+}: {
+  agency?: Agency
+  queryStateProp?: string
+}): ReactElement => {
+  const [queryState, setQueryState] = useState(queryStateProp)
+  const styles = useMultiStyleConfig('OptionsMenu', {})
 
   const accordionRef: LegacyRef<HTMLButtonElement> = createRef()
 
