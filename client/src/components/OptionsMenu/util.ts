591e47d3ce2453fe45a181b45a626a869374bbde
zx <67887489+tan-zx@users.noreply.github.com>
2022-03-03 11:33:18 +0800

    Use alias import in util.ts

--- client/src/components/OptionsMenu/util.ts
+++ client/src/components/OptionsMenu/util.ts
@@ -1,5 +1,5 @@
 import { Topic } from '~shared/types/base'
-import { Agency } from '../../services/AgencyService'
+import { Agency } from '~shared/types/base'
 import { GetTopicsDto } from '../../api'
 
 type bySpecifiedOrderComparator =
