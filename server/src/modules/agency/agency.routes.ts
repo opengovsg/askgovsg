64540350ee32506458038ac32597fbf3c7696d66
Nigel Ng <37100124+siuting0511@users.noreply.github.com>
2022-03-18 15:36:33 +0800

    Update server/src/modules/agency/agency.routes.ts
    Co-authored-by: LoneRifle <LoneRifle@users.noreply.github.com>

--- server/src/modules/agency/agency.routes.ts
+++ server/src/modules/agency/agency.routes.ts
@@ -14,7 +14,7 @@ export const routeAgencies = ({
 
   /**
    * List all agencies
-   * @route  GET /api/agencies/all
+   * @route  GET /api/agencies
    * @return 200 with agency
    * @return 404 if agency is not found
    * @return 500 if database error
