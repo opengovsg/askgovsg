e58c202614abd4e12ddce65b08cfb6868bb3239f
siuting0511 <siuting0511@gmail.com>
2022-03-22 15:32:13 +0800

    feat: add 'getListOfAllAgencies' to AgencyService

--- client/src/services/AgencyService.ts
+++ client/src/services/AgencyService.ts
@@ -1,10 +1,13 @@
 import { Agency } from '~shared/types/base'
 import { ApiClient } from '../api'
-
 export { Agency } from '~shared/types/base'
 
 export type AgencyQuery = {
   shortname: string
+}
+
+export const getListOfAllAgencies = (): Promise<Agency[]> => {
+  return ApiClient.get<Agency[]>('/').then(({ data }) => data)
 }
 
 export const getAgencyByShortName = (query: AgencyQuery): Promise<Agency> => {
@@ -23,6 +26,7 @@ export const listAgencyShortNames = (): Promise<{ shortname: string }[]> => {
   )
 }
 
+export const GET_LIST_OF_ALL_AGENCIES = 'getListOfAllAgencies'
 export const GET_AGENCY_BY_SHORTNAME_QUERY_KEY = 'getAgencyByShortName'
 export const GET_AGENCY_BY_ID_QUERY_KEY = 'getAgencyById'
 export const LIST_AGENCY_SHORTNAMES = 'listAgencyShortNames'
