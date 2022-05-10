f8b6735d857f163e819997365646b54b92055568
zx <67887489+tan-zx@users.noreply.github.com>
2022-05-10 10:17:54 +0800

    refactor: localize HomePageProvider, use custom hooks (#1594)

--- client/src/pages/HomePage/HomePage.component.tsx
+++ client/src/pages/HomePage/HomePage.component.tsx
@@ -3,36 +3,39 @@ import { Flex, HStack, Spacer } from '@chakra-ui/react'
 import CitizenRequest from '../../components/CitizenRequest/CitizenRequest.component'
 import OptionsMenu from '../../components/OptionsMenu/OptionsMenu.component'
 import { Questions } from '../../components/Questions/Questions.component'
+import { HomePageProvider } from '../../contexts/HomePageContext'
 
 const HomePage = (): JSX.Element => {
   return (
-    <Flex direction="column" height="100%" id="home-page">
-      <OptionsMenu />
-      <HStack
-        id="main"
-        alignItems="flex-start"
-        display="grid"
-        gridTemplateColumns={{
-          base: '1fr',
-          xl: '1fr',
-        }}
-      >
-        <Flex
-          id="questions"
-          maxW="680px"
-          m="auto"
-          justifySelf="center"
-          w="100%"
-          pt={{ sm: '30px', xl: '22px' }}
-          px={{ base: 8, md: 0 }}
-          direction={{ base: 'column', lg: 'row' }}
+    <HomePageProvider>
+      <Flex direction="column" height="100%" id="home-page">
+        <OptionsMenu />
+        <HStack
+          id="main"
+          alignItems="flex-start"
+          display="grid"
+          gridTemplateColumns={{
+            base: '1fr',
+            xl: '1fr',
+          }}
         >
-          <Questions />
-        </Flex>
-      </HStack>
-      <Spacer />
-      <CitizenRequest />
-    </Flex>
+          <Flex
+            id="questions"
+            maxW="680px"
+            m="auto"
+            justifySelf="center"
+            w="100%"
+            pt={{ sm: '30px', xl: '22px' }}
+            px={{ base: 8, md: 0 }}
+            direction={{ base: 'column', lg: 'row' }}
+          >
+            <Questions />
+          </Flex>
+        </HStack>
+        <Spacer />
+        <CitizenRequest />
+      </Flex>
+    </HomePageProvider>
   )
 }
 
